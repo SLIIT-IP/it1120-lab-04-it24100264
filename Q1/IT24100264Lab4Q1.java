8dbd839f5708f2caca1c0899ba65fbec11190e5f
it24100264 <it24100264@my.sliit.lk>
2024-08-18 12:01:54 +0530

    Update and rename IT24100353Lab3Q1A.java to IT24100264Lab4Q1.java

---
+++ Q1/IT24100264Lab4Q1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IT24100264Lab4Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Enter a number: ");
+        float number =scanner.nextFloat();
+
+	if (number > 0){
+	System.out.println("THe number is:positive");}
+
+	else if (number < 0){
+	System.out.println("THe number is:negative");}
+
+	else {
+	System.out.println("THe number is:Zero");}
+	}
+}
+	
+		
